118fef757e3b9c74f3e8605e11e05f0fec742e33
Nick Bansal <Bansal321@Nicks-iMac.local>
2018-11-22 14:33:27 +0000

    Form styled

--- src/component/NewForm.jsx
+++ src/component/NewForm.jsx
@@ -1,61 +1,26 @@
 import React from 'react';
+import './Form.css'
 
 const NewForm = () => {
-    const formStyle = {
-        display: 'flex',
-        justifyContent: 'center'
-    }
-
-    const formItems = {
-        display: 'flex',
-        width: '550px',
-        height: '350px',
-        padding: '0'
-    }
-
-    const buttonStyle = {
-        padding: '19px 39px 18px 39px',
-        color: '#FFF',
-        background: '#4bc970',
-        fontSize: '18px',
-        textAlign: 'center',
-        fontStyle: 'normal',
-        borderRadius: '5px',
-        width: '70%',
-        border: '1px solid #3ac162',
-        borderWidth: '1px 1px 3px',
-        boxShadow: '0 -1px 0 rgba(255, 255, 255, 0.1)',
-        margin: 'auto'
-    }
-
-    const inputStyle = {
-        margin: 'auto',
-        width: '80%'
-    }
-
-    const titleStyle = {
-        background: '#4bc970',
-        margin: '0',
-        height: '15%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+   
 
     return (
         <div>
-            <h1>Form</h1>
-            <form style={formStyle}>
-                <fieldset style={formItems}>
-                    <p style={titleStyle}>Login to the Coaching Manual</p>
+            <form className="formStyle">
+                <fieldset className="formItems">
+                    <p className="titleStyle"><i className="icons" class="fas fa-key"></i>Login to the Coaching Manual</p>
 
-                    <label for="email">Email address</label>
-                    <input style={inputStyle} type="email" placeholder="Please enter your email..." id="email" required />
+                    <label className="labelStyle" for="email">Email address:</label>
+                    <input className="inputStyle" type="email" placeholder="Please enter your email..." id="email" required />
 
-                    <label for="email">Password</label>
-                    <input style={inputStyle} type="password" placeholder="Password" id="email" required />
+                    <label className="labelStyle" for="email">Password:</label>
+                    <input className="inputStyle" type="password" placeholder="Password" id="email" required />
 
-                    <button style={buttonStyle} type="submit">Submit</button>
+                    <button className="buttonStyle" type="submit">Submit</button>
+                    <div className="bottomWording">
+                        <p className="newColor Hovering">Forgot Password?</p>
+                        <p>No account? <span className="newColor Hovering">Please sign up</span></p>
+                    </div>
                 </fieldset>
             </form>
         </div>
